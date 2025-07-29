34f0fa4c48fbb7e6232a505b3646c030b466f887
Bhavy <bhavyajain817@gmail.com>
2025-07-29 12:09:23 +0530

    changes

--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -10,13 +10,13 @@ import { getAuth } from "firebase/auth";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyBEll1RbwLBpY5aJfTi_jZxmIH-SpVRwrs",
-  authDomain: "globatonn.firebaseapp.com",
-  projectId: "globatonn",
-  storageBucket: "globatonn.firebasestorage.app",
-  messagingSenderId: "1048764388256",
-  appId: "1:1048764388256:web:5810198f24e46a1e9e4ac5",
-  measurementId: "G-VFMQRQMWJZ"
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+  measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID
 };
 
 // Initialize Firebase
